13588d26b8758b3e0c070c51a998f60d380364e3
Kamil Sikora <kamil.sikora@swmansion.com>
2026-01-13 15:45:24 +0100

    fix: exported module and type

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ export type {
   DetourContextType,
   DetourStorage,
 } from './links/types/index';
+
+export { DetourAnalytics } from './analytics/analytics';
+export type { DetourEventNames } from './analytics/types/index';
