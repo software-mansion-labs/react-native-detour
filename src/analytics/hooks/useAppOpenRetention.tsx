77c24be902e4d69a18b5d793e9044a505c6e1221
Kamil Sikora <kamil.sikora@swmansion.com>
2026-02-19 17:03:57 +0100

    fix: prevent logging app_open on fast refresh

--- src/analytics/hooks/useAppOpenRetention.tsx
+++ src/analytics/hooks/useAppOpenRetention.tsx
@@ -2,9 +2,13 @@ import { useEffect } from 'react';
 import { logRetention } from '../analytics';
 
 const DEFAULT_RETENTION_EVENT = 'app_open';
+let isColdStartLogged = false;
 
 export const useAppOpenRetention = () => {
   useEffect(() => {
-    logRetention(DEFAULT_RETENTION_EVENT);
+    if (!isColdStartLogged) {
+      logRetention(DEFAULT_RETENTION_EVENT);
+      isColdStartLogged = true;
+    }
   }, []);
 };
