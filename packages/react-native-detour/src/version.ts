1f26a6aabdc37678d0c242d05afa2cf1801cfdc6
Kamil Sikora <kamil.sikora@swmansion.com>
2026-05-05 12:37:08 +0200

    chore: release 2.2.0

--- packages/react-native-detour/src/version.ts
+++ packages/react-native-detour/src/version.ts
@@ -1,2 +1,2 @@
-export const version = "2.1.1"; // This line will is updated automatically by releasing script
+export const version = "2.2.0"; // This line will is updated automatically by releasing script
 export const SDK_HEADER_VALUE = `react-native/${version}`;
