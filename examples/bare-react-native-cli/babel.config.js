1f3e35337f0a10e997b036cfb73fec0a3f5c542a
Sebastian Piaskowy <sebastian.piaskowy@swmansion.com>
2026-06-22 12:05:52 +0200

    refactor: remove bare-react-native-cli example

--- examples/bare-react-native-cli/babel.config.js
+++
@@ -1,6 +0,0 @@
-// `babel-preset-expo` (a superset of `@react-native/babel-preset`) pairs with
-// `expo/metro-config` and enables inlining of `process.env.EXPO_PUBLIC_*` vars.
-module.exports = function (api) {
-  api.cache(true);
-  return { presets: ["babel-preset-expo"] };
-};
